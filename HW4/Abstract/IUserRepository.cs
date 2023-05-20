6c3a07d9ef1228e4fc3d556ace5a19823feb9586
M.Asadollahi <asadolahi.ma7@gmail.com>
2023-05-20 19:34:22 +0430

    The IUserRepository interface and CSVAccess, User, UserRepository classes and User.csv file were created

---
+++ HW4/Abstract/IUserRepository.cs
@@ -0,0 +1,21 @@
+﻿using HW4.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW4.Abstract
+{
+    public interface IUserRepository
+    {
+        bool Create(User user);
+
+        bool Delete(User user);
+
+        List<User> GetAllUsers();
+
+        bool Update(User oldUser, User newUser);
+
+    }
+}
---
+++ HW4/Repository/UserRepository.cs
@@ -0,0 +1,33 @@
+﻿using HW4.Abstract;
+using HW4.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW4.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        public bool Create(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<User> GetAllUsers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(User oldUser, User newUser)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
