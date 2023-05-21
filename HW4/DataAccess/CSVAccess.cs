88bbe1dd02d21de319d3e0af866f6732fea20bbe
M.Asadollahi <asadolahi.ma7@gmail.com>
2023-05-21 13:25:04 +0430

    4 records were added to the User.csv file and also the GetAllUsers method was written into the CSVAccess class.

--- HW4/DataAccess/CSVAccess.cs
+++ HW4/DataAccess/CSVAccess.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using CsvHelper.Configuration;
+using CsvHelper;
+using HW4.Model;
+using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +12,36 @@ namespace HW4.DataAccess
 {
     public class CSVAccess
     {
+        string? baseDirectoryPath = string.Empty;
+        string? dataFolderPath = string.Empty;
+        string? csvFilePath = string.Empty;
 
+        public List<User> GetAllUsers()
+        {
+            baseDirectoryPath = Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory)?
+                         .Parent?.Parent?.Parent?.FullName;
+            dataFolderPath = Path.Combine(baseDirectoryPath, "Data");
+            csvFilePath = Path.Combine(dataFolderPath, "Users.csv");
+
+
+            var Configuration = new CsvConfiguration(CultureInfo.InvariantCulture)
+            {
+                Encoding = Encoding.UTF8,
+                Delimiter = ","
+            };
+
+            using (FileStream fs = new FileStream(csvFilePath, FileMode.Open))
+            {
+                using (TextReader reader = new StreamReader(fs, Encoding.UTF8))
+                {
+                    using (var csv = new CsvReader(reader, Configuration))
+                    {
+
+                        var data = csv.GetRecords<User>().ToList();
+                        return data;
+                    }
+                }
+            }
+        }
     }
 }
